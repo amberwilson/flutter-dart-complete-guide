319c1a6caa15565e631026dbc0cbb8d4fea7af3f
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-04-30 18:36:43 -0300

    Add a hardcoded message to chat via button

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -29,7 +29,11 @@ class ChatScreen extends StatelessWidget {
           }),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/SmJIvPjMsCfl6jNMlJ5w/messages')
+              .add({'text': 'a new message'});
+        },
       ),
     );
   }
