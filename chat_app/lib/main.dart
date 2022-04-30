af8b12f3f9ca8ae6d3bf55573cbd4d072dd58b13
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-04-30 18:58:44 -0300

    Add auth form inputs

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/widgets/auth/auth_form.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ChatScreen(),
+      home: const AuthForm(),
     );
   }
 }
