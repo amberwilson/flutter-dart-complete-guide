bee1d394c1313aa92f8876fef04dcc4e29fe0267
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-07 14:54:07 -0300

    Initialize to use Firebase emulators

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,14 +1,41 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/auth_screen.dart';
 import 'screens/chat_screen.dart';
 
+const bool USE_EMULATOR = true;
+
+/// Connect to the firebase emulator for required services
+Future _connectToFirebaseEmulator() async {
+  final localHostString = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+
+  FirebaseFirestore.instance.settings = Settings(
+    host: '$localHostString:8080',
+    sslEnabled: false,
+    persistenceEnabled: false,
+  );
+
+  await FirebaseAuth.instance.useAuthEmulator(localHostString, 9099);
+  FirebaseFirestore.instance.useFirestoreEmulator(localHostString, 8080);
+  FirebaseStorage.instance.useStorageEmulator(localHostString, 9199);
+  FirebaseFunctions.instance.useFunctionsEmulator(localHostString, 5001);
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+
+  if (USE_EMULATOR) {
+    await _connectToFirebaseEmulator();
+  }
 
   runApp(const MyApp());
 }
